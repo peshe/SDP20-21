80f0bc1570fa3c3d88b9874511de195147999785
Yasen Alexiev <yassen4o222@abv.bg>
2020-11-09 13:08:47 +0200

    Update benchmark.hpp

--- exercises/2&4/Utils/Benchmark/benchmark.hpp
+++ exercises/2&4/Utils/Benchmark/benchmark.hpp
@@ -7,8 +7,13 @@
 
 namespace dsa
 {
-	enum class T_FORMAT
+	enum T_FORMAT
 	{
+		// 0001
+		// 0010
+		// 0100
+		// 1000
+		// 1111
 		F_MICRO	= 1, //!< format the time in microseconds => 1000 microseconds = 1 millisecond 
 		F_MILLI	= 2, //!< format the time in milliseconds => 1000 milliseconds = 1 second 
 		F_SEC	= 4,   //!< format the time in seconds      => 60 seconds = 1 minute
@@ -18,29 +23,32 @@ namespace dsa
 
 	template <typename Func>
 	void benchmarking_this_function( const Func& f, T_FORMAT format = T_FORMAT::F_MICRO,
-										std::string name = "", std::ostream os = std::cout )
+										std::string name = "", std::ostream& os = std::cout )
 	{
-		std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
+		using namespace std::chrono;
+		time_point<high_resolution_clock> start = high_resolution_clock::now();
 
 		f();
 
-		std::chrono::time_point<std::chrono::high_resolution_clock> end = std::chrono::high_resolution_clock::now();
+		time_point<high_resolution_clock> end = high_resolution_clock::now();
+		
+		if ( !name.empty() ) os << '[' << name << "] " << std::endl;
 
-		if ( format & F_MICRO )
+		if ( format & T_FORMAT::F_MICRO )
 		{
-			os << std::chrono::duration_cast<std::chrono::microseconds>( start - end ).count() << std::endl;
+			os << duration_cast<microseconds>( end - start ).count() << " microseconds" << std::endl;
 		}
-		if ( format & F_MILLI )
+		if ( format & T_FORMAT::F_MILLI )
 		{
-			os << std::chrono::duration_cast<std::chrono::milliseconds>( start - end ).count() << std::endl;
+			os << duration_cast<milliseconds>( end - start ).count() << " milliseconds" << std::endl;
 		}
-		if ( format & F_SEC )
+		if ( format & T_FORMAT::F_SEC )
 		{
-			os << std::chrono::duration_cast<std::chrono::seconds>( start - end ).count() << std::endl;
+			os << duration_cast<seconds>( end - start ).count() << " seconds" << std::endl;
 		}
-		if ( format & F_MIN )
+		if ( format & T_FORMAT::F_MIN )
 		{
-			os << std::chrono::duration_cast<std::chrono::minutes>( start - end ).count() << std::endl;
+			os << duration_cast<minutes>( end - start ).count() << " minutes" << std::endl;
 		}
 
 	}
